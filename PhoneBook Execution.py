f19519746a5a8415d836ef06b0777b012492b1ed
MarcShaXie <marcshaxie@gmail.com>
2022-08-05 18:35:47 -0400

    init check in

---
+++ PhoneBook Execution.py
@@ -0,0 +1,43 @@
+from PhoneBookClass import Entry
+from PhoneBookClass import PhoneBook
+
+Phonebook = PhoneBook()
+
+print("Welcome")
+
+
+
+while True:
+    print("What would you like to do whith your contact list?")
+    print("Enter 1 to add a contact, 2 to delete one, 3 to look up a contact or 4 to see your contact list")
+    user_operation = input()
+    if user_operation == "1":
+        print("Enter a name for the contact")
+        name = input()
+        print("Enter a phone number for the contact")
+        phonenumber = input()
+        Phonebook.insert(name, phonenumber)
+        Phonebook.sort
+    elif user_operation == "2":
+        print("Enter the name of the contact you would like to delete")
+        name = input()
+        Phonebook.delete(name)
+    elif user_operation == "3":
+        print("Enter 1 to search by name or 2 to search by number")
+        searchbymethod = input()
+        if searchbymethod == "1":
+            print("Enter the name of the contact you are searching for")
+            name = input()
+            print("The phone number belonging to inputed name is")
+            print(Phonebook.searchbyname(name))
+        elif searchbymethod == "2":
+            print("Enter phone number of the contact you are searching for")
+            phonenumber = input()
+            print("The owner of this phone number is")
+            print(Phonebook.searchbyphonenumber(phonenumber))
+        else:
+            print("invalid input you are now being returned to the main menu")
+    elif user_operation == "4":
+        Phonebook.print()
+    else:
+        print("invalid input")
